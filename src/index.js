a6900f293ff77ce6d36eefa64d21159acbf461f5
unknown <utkarsh.junglebeast@gmail.com>
2020-10-02 18:30:45 +0530

    Keeper Layout New

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App.jsx";
+import App from "./components/App";
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById("root")
 );
